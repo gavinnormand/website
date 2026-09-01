a07787f9e3afb19f2f72a9c0152f3158be3f4ebe
gavinnormand <normandgavin@gmail.com>
2026-08-31 23:21:56 -0400

    update card styling

--- src/pages/Card.tsx
+++ src/pages/Card.tsx
@@ -1,21 +1,32 @@
 function Card() {
   return (
-    <div className="bg-background-card h-screen w-full card-wrapper">
-      <svg className="absolute inset-0 h-full w-full rounded-sm opacity-[0.15] mix-blend-multiply">
+    <div className="bg-background-card card-wrapper h-screen w-full">
+      <svg className="pointer-events-none absolute inset-0 h-full w-full rounded-sm opacity-[0.15] mix-blend-multiply">
         <filter id="paperNoise">
           <feTurbulence
             type="fractalNoise"
-            baseFrequency="0.9"
-            numOctaves="4"
+            baseFrequency="0.04"
+            numOctaves="5"
+            seed="7"
             stitchTiles="stitch"
+            result="noise"
           />
-          <feColorMatrix type="saturate" values="0" />
+
+          <feDiffuseLighting
+            in="contrastNoise"
+            result="lit"
+            surfaceScale="3"
+            diffuseConstant="1.2"
+            lightingColor="white"
+          >
+            <feDistantLight azimuth="235" elevation="40" />
+          </feDiffuseLighting>
         </filter>
         <rect width="100%" height="100%" filter="url(#paperNoise)" />
       </svg>
-      <div className="font-copperplate flex h-full w-full flex-col items-center justify-between p-8 text-lg">
+      <div className="font-copperplate flex h-full w-full flex-col items-center justify-between p-8">
         <div className="flex w-full flex-row justify-between">
-          <p>908.415.5997</p>
+          <a href="tel:+19084155997">908.415.5997</a>
           <div className="flex flex-col items-center">
             <p>KHOURY COLLEGE</p>
             <p>Northeastern University</p>
@@ -23,10 +34,15 @@ function Card() {
         </div>
         <div className="flex flex-col items-center">
           <p className="text-3xl font-bold">Gavin NORMAND</p>
-          <p>Honors CS @ Northeastern</p>
+          <p className="text-lg">Honors CS @ Northeastern</p>
         </div>
         <div className="flex w-full flex-row justify-center">
-          <p>normandgavin@gmail.com | gavinnormand.com</p>
+          <p>
+            <a href="mailto:normandgavin@gmail.com">normandgavin@gmail.com</a> |{" "}
+            <a href="https://www.gavinnormand.com/" target="_blank">
+              gavinnormand.com
+            </a>
+          </p>
         </div>
       </div>
     </div>
