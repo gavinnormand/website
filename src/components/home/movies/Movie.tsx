dbdb104be9b72ef5ce8a03527ab2426a51d9e866
gavinnormand <normandgavin@gmail.com>
2026-09-03 23:18:14 -0400

    Remove buggy 'show more' on letterboxd long reviews

--- src/components/home/movies/Movie.tsx
+++ src/components/home/movies/Movie.tsx
@@ -1,27 +1,9 @@
-import { useEffect, useRef, useState } from "react";
 import type { MovieType } from "../../../types/MovieType";
 import Rating from "./Rating";
 
 function Movie({ movie }: { movie: MovieType }) {
-  const [truncated, setTruncated] = useState<boolean>(true);
-  const [isOverflowing, setIsOverflowing] = useState<boolean>(false);
-  const descriptionRef = useRef<HTMLParagraphElement>(null);
-
-  useEffect(() => {
-    const checkOverflow = () => {
-      const el = descriptionRef.current;
-      if (el) {
-        setIsOverflowing(el.scrollWidth > el.clientWidth);
-      }
-    };
-
-    checkOverflow();
-    window.addEventListener("resize", checkOverflow);
-    return () => window.removeEventListener("resize", checkOverflow);
-  }, [movie.description]);
-
   return (
-    <div className="flex flex-row items-center justify-between gap-2">
+    <div className="flex flex-row items-center justify-between gap-6">
       <div className="flex min-w-0 flex-1 flex-row items-center gap-6">
         <img src={movie.posterURL} className="w-14 rounded-md" />
         <div className="flex min-w-0 flex-col gap-1">
@@ -36,22 +18,7 @@ function Movie({ movie }: { movie: MovieType }) {
             <p>&bull;</p>
             <Rating rating={movie.rating} />
           </div>
-          <div>
-            <p
-              ref={descriptionRef}
-              className={`${truncated ? "truncate" : ""}`}
-            >
-              {movie.description}
-            </p>
-            {isOverflowing && (
-              <p
-                className="cursor-pointer underline w-fit"
-                onClick={() => setTruncated(!truncated)}
-              >
-                {truncated ? "see more" : "see less"}
-              </p>
-            )}
-          </div>
+          <p className="truncate">{movie.description}</p>
         </div>
       </div>
       <p className="hidden font-mono md:block">
